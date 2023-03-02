ea7d1e7be66d18934a818870504eadcf8ee2a739
Danchik452 <fisherd2004@gmail.com>
2023-03-02 11:14:45 +0700

    Initial commit.

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+Group:
+Ivanov Ivan
+'''
+
+def triangle (x, y, a, b, angle, color):
+    '''
+    
+    :param x: 
+    :param y: 
+    :param a: 
+    :param b: 
+    :param angle: 
+    :param color: 
+    :return: 
+    '''''
